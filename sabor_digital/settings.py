36ae25c45f0425f0afc676690fcc4f84519cb476
Adilson G Andrade <adilsongandrade@gmail.com>
2025-09-20 20:14:37 -0300

    Update Debug e Hosts Local e Production (Vercel)
    Ajustando o DEBUG local x ambiente Desenvolvimento e Produção. Com essa alteração, o ALLOWED_HOSTS irá incluir 127.0.0.1 e localhost apenas quando o DEBUG estiver ativado no seu ambiente local, resolvendo o erro Bad Request. A configuração da Vercel continuará funcionando, pois ela não terá a variável DEBUG definida (ou a terá como False), e o código irá usar apenas o ALLOWED_HOSTS de produção

--- sabor_digital/settings.py
+++ sabor_digital/settings.py
@@ -35,8 +35,11 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 #DEBUG = True
 
 #ALLOWED_HOSTS = []
-DEBUG = False
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
+
 ALLOWED_HOSTS = ['pi-2-sabor-digital.vercel.app']
+if DEBUG:
+    ALLOWED_HOSTS += ['127.0.0.1', 'localhost']
 
 
 # Application definition
