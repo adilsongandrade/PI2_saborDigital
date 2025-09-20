7988d28ba099570d136019625b183ec893a29e49
Adilson G Andrade <adilsongandrade@gmail.com>
2025-09-20 20:46:00 -0300

    Update para tratar arquivos estáticos (CSS)
    O Django serve os arquivos estáticos diretamente, pois o modo de depuração (DEBUG=True) está ativado. Em produção, com DEBUG=False, o Django não serve os arquivos estáticos. A responsabilidade de servir esses arquivos passa para um servidor web dedicado (como Nginx ou Apache). No contexto da Vercel, isso exige uma configuração especial. Necessário instruir a Vercel a rodar o comando collectstatic e a servir a pasta onde os arquivos são coletados.

--- sabor_digital/settings.py
+++ sabor_digital/settings.py
@@ -138,3 +138,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.2/howto/static-files/
+
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root') # Adiciona esta linha
