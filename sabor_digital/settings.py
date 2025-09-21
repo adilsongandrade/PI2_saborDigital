1ba77ffce25224c08788f07d474f1d040664ba1f
Adilson G Andrade <adilsongandrade@gmail.com>
2025-09-20 21:10:52 -0300

    Update para static rotes

--- sabor_digital/settings.py
+++ sabor_digital/settings.py
@@ -19,6 +19,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # ... (outros imports e configurações)
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
