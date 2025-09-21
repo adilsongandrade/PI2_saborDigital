2b77d9186f60672b5657b878fce2a170601e7816
Adilson G Andrade <adilsongandrade@gmail.com>
2025-09-20 21:13:47 -0300

    update reposicionando static_root
    Reposicionando a linha de STATIC_ROOT esteja abaixo da definição de BASE_DIR.

--- sabor_digital/settings.py
+++ sabor_digital/settings.py
@@ -18,13 +18,11 @@ from dotenv import load_dotenv
 # Carrega as variáveis do arquivo .env
 load_dotenv()
 
-# ... (outros imports e configurações)
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
