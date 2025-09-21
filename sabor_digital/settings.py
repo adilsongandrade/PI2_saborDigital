ae7ecc62ecddfd910e07d77045269744545c2807
Adilson G Andrade <adilsongandrade@gmail.com>
2025-09-20 21:42:54 -0300

    update corrigindo static path

--- sabor_digital/settings.py
+++ sabor_digital/settings.py
@@ -21,9 +21,6 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -142,5 +139,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root') # Adiciona esta linha
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
