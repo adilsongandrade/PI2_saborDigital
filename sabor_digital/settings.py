6bd5723b65adb4e3032549d8617f410f41f795ea
Adilson G Andrade <adilsongandrade@gmail.com>
2025-09-22 10:58:17 -0300

    Recommit all system for vercel public static

--- sabor_digital/settings.py
+++ sabor_digital/settings.py
@@ -135,10 +135,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / "usuarios" / "static",
-]
-
+STATICFILES_DIRS = [BASE_DIR / "public" / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
